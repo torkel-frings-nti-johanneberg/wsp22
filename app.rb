00f1da88d162505b20d2477ff976cc29b991ae4d
torkel-frings-nti-johanneberg <74763805+torkel-frings-nti-johanneberg@users.noreply.github.com>
2022-04-15 12:20:26 +0200

    completetd all funktionalety

--- app.rb
+++ app.rb
@@ -13,28 +13,55 @@ get('/') do
 end
 
 get('/home') do
-    hej()
+    hej(1)
     slim(:"home")
 end
 
 get ('/books') do
-    getTittleNumberBooks()
+    getBooksInfo()
     slim(:"book/index")
 end
 
 get ('/book/new') do
+    if rolllrvel() != nil
+        redirect to("/home")
+    end
     slim(:"book/new")
 end
 
+get '/book/edit' do
+    if rolllrvel() <= 1
+        redirect to("/home")
+    end
+    editLinks()
+    slim(:"book/edit/index")
+end
+
+get '/books/:id/edit' do
+    if rolllrvel() < 1
+        redirect to("/home")
+    end
+    slim(:"book/edit/edit")
+end
+
 get ('/place/new') do
+    if rolllrvel() != nil
+        redirect to("/home")
+    end
     slim(:"place/new")
 end
 
 get ('/pepol/new') do
+    if rolllrvel() != nil
+        redirect to("/home")
+    end
     slim(:"pepol/new")
 end
 
 post ('/places') do
+    if rolllrvel() != nil
+        redirect to("/home")
+    end
     book_num = params[:book]
 
     book_id = bookId(book_num)
@@ -48,6 +75,9 @@ post ('/places') do
 end
 
 post ('/pepol') do
+    if rolllrvel() != nil
+        redirect to("/home")
+    end
     book_num = params[:book]
 
     book_id = bookId(book_num)
@@ -65,6 +95,9 @@ post ('/pepol') do
 end
 
 post ('/books') do
+    if rolllrvel() != nil
+        redirect to("/home")
+    end
     title =  params[:title]
     number =  params[:number]
     publiDate = params[:pub_date]
@@ -73,8 +106,9 @@ post ('/books') do
     boar = params[:boar]
     menhirs = params[:stone]
     place = params[:place]
+    artist = params[:artist]
 
-    addbooks(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs)
+    addbooks(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs, artist)
 end
 
 get("/login") do
@@ -95,11 +129,14 @@ post("/login") do
     password = params[:password]
     db = db_loder()
     result = db.execute("SELECT * FROM user WHERE name = ?",username).first
+    if result == nil
+        redirect to('/error')
+    end
     pwdigets = result["pasword"]
     id = result["id"]
-    hej()
+    hej(1)
     p result
-    hej()
+    hej(1)
     if BCrypt::Password.new(pwdigets) == password
         p id
         session[:id] = id
@@ -129,13 +166,107 @@ post("/users") do
     end
 end
 
+get '/rool/update' do
+    @listOfreaders = getReaderList()
+    slim(:"users/update")
+end
+
+get '/rool/:id/update' do
+    if rolllrvel() <= 2
+        redirect to("/home")
+    end
+    user_id = params[:id]
+    promote(user_id)
+end
+
+get '/rool/edit' do
+    @listOfreaders = getUserList()
+    hej(8)
+    slim(:"users/update")
+end
+
+post('/rool/:id/update') do
+    id = params[:id]
+    rool = params[:rool]   
+    moderate(id,rool)
+end
+
+get '/user/edit' do
+
+    slim(:"users/edit")
+end
+
+post('/user/:id/delete') do
+    user_id = params[:id]
+    deletUser(user_id)
+    redirect to("/home")
+end
+
+post("/book/:id/edit") do
+    if rolllrvel() <= 1
+        redirect to("/home")
+    end
+    title =  params[:title]
+    number =  params[:number]
+    publiDate = params[:pub_date]
+    posionsDrunk = params[:potions]
+    piratShipSunk = params[:pirats]
+    boar = params[:boar]
+    menhirs = params[:stone]
+    place = params[:place]
+    artist = params[:artist]
+    id = params[:id]
+
+    editbook(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs, artist, id)
+end
+
+post("/pepol_in_book/:id/edit") do
+    if rolllrvel() <= 1
+        redirect to("/home")
+    end
+    book_id = params[:id]
+    pepolAdd = params[:pepolAdd]
+    pepolRemove = params[:pepolRemove]
+    pepolAdd = pepolAdd.split("\n")
+    pepolRemove = pepolRemove.split("\n")
+
+    pepolAdd.map! {|person| person.chomp}
+    pepolRemove.map! {|person| person.chomp}
+
+    
+    pepolAdd.each do |person|
+        addPerson(person,book_id)
+    end
+    pepolRemove.each do |person|
+        removePerson(person,book_id)
+    end
+    redirect to("/home")
+end
+
+post("/book_in_placese/:id/edit") do
+    if rolllrvel() <= 1
+        redirect to("/home")
+    end
+    book_id = params[:id]
+    placeAdd = params[:placeAdd]
+    placeRemove = params[:placeRemove]
+    placeAdd = placeAdd.split("\n")
+    placeRemove = placeRemove.split("\n")
+
+    placeAdd.map! {|place| place.chomp}
+    placeRemove.map! {|place| place.chomp}
+
+    
+    placeAdd.each do |place|
+        addPlace(place,book_id)
+    end
+    placeRemove.each do |place|
+        removePlace(place,book_id)
+    end
+    redirect to("/home")
+end
+
 get ('/error') do
     slim(:error)
 end
 
-def db_loder()
-    db = SQLite3::Database.new("db/slut_prodj_wsp_22.db")
-    db.results_as_hash = true
-    
-    return db
-end
--- model.rb
+++ model.rb
@@ -40,32 +40,97 @@ def addPlace(place,book_id)
         end
     end
     # p place_id
-    db.execute('INSERT INTO book_set_in_place (book_id,palce_id) VALUES (?,?)',book_id,place_id)
-    if newPlase
-        db.execute('INSERT INTO places (name) VALUES (?)',place)
+    existing = db.execute('SELECT * FROM book_set_in_place WHERE book_id=? AND palce_id=?',book_id,place_id)
+    hej(4)
+    p existing
+    if existing.length == 0
+      db.execute('INSERT INTO book_set_in_place (book_id,palce_id) VALUES (?,?)',book_id,place_id)
+      if newPlase
+          db.execute('INSERT INTO places (name) VALUES (?)',place)
+      end
+    else
+      redirect("/error")
     end
+
+    
 end
 
 def addPerson(person,book_id)
   person = person.capitalize()
   db = db_loder()
   pepol_id = db.execute('SELECT id FROM pepol').last
+  # hej()
+  # hej()
+  # hej()
+  # p pepol_id
   pepol_id = pepol_id["id"] + 1
+  # p pepol_id
   listOfPepol = db.execute('SELECT name FROM pepol')
+  # p listOfPepol
   newPerson = true
   listOfPepol.each do |oldpepol|
-      if oldpepol["name"] == person
+      # p "______________________________-"
+      # p oldpepol["name"] == person
+      # p oldpepol["name"] 
+      # p person
+      if oldpepol["name"] == person 
           pepol_id = db.execute('SELECT id FROM pepol WHERE name IS ?',person).first
           pepol_id = pepol_id["id"]
           newPerson = false
       end
   end
   # p place_id
-  db.execute('INSERT INTO pepol_in_book (book_id,pepol_id) VALUES (?,?)',book_id,pepol_id)
-  if newPlase
-      db.execute('INSERT INTO pepol (name) VALUES (?)',person)
+  existing = db.execute('SELECT * FROM pepol_in_book WHERE book_id=? AND pepol_id=?',book_id,pepol_id)
+  hej(4)
+  p existing
+  if existing.length == 0
+    db.execute('INSERT INTO pepol_in_book (book_id,pepol_id) VALUES (?,?)',book_id,pepol_id)
+    if newPerson
+        db.execute('INSERT INTO pepol (name) VALUES (?)',person)
+    end
+  else
+    redirect to("/error")
   end
 end
+
+def removePerson(person,book_id)
+  person = person.capitalize()
+  db = db_loder()
+  listOfPepol = db.execute('SELECT name FROM pepol')
+
+  listOfPepol.each do |oldpepol|
+    if oldpepol["name"] == person
+      pepol_id = db.execute('SELECT id FROM pepol WHERE name IS ?',person).first
+      hej(2)
+      p pepol_id["id"]
+      pepol_id = pepol_id["id"]
+      p book_id
+      db.execute('DELETE FROM pepol_in_book WHERE book_id=? AND pepol_id=?',book_id,pepol_id)
+    end
+  end
+
+  redirect("/home")
+end
+
+def removePlace(place,book_id)
+  place = place.capitalize()
+  db = db_loder()
+  listOfPlaces = db.execute('SELECT name FROM places')
+
+  listOfPlaces.each do |oldplaces|
+    if oldplaces["name"] == place
+      place_id = db.execute('SELECT id FROM places WHERE name IS ?',place).first
+      # hej(2)
+
+      place_id = place_id["id"]
+
+      db.execute('DELETE FROM book_set_in_place WHERE book_id=? AND palce_id=?',book_id,place_id)
+    end
+  end
+
+  
+end
+
 
 def bookId(bNum)
     db = db_loder()
@@ -74,7 +139,7 @@ def bookId(bNum)
     return book_id["id"].to_i
 end
 
-def addbook(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs)
+def addbook(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs, artist)
   db = db_loder()
   book_id =db.execute('SELECT id FROM book').last
   book_id = book_id["id"]
@@ -83,7 +148,7 @@ def addbook(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs
   # get artist_id
   artists = db.execute("SELECT * FROM artist")
   artists_id = 0
-  cunrentArtist = params[:artist]
+  cunrentArtist = artist
   artists.each do |artist|
       if artist["name"] == cunrentArtist
           artists_id = artist["id"]
@@ -96,16 +161,23 @@ def addbook(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs
   # add book info to db
   db.execute("INSERT INTO book (title, number, publish_date,posions_drunk,pirat_ship_shunk,wild_boar,menhirs,artist_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs, artists_id)
 
-  redirect("/")
+  redirect("/home")
 end
 
-def getTittleNumberBooks()
+def getBooksInfo()
   db = db_loder()
-  @booklist = db.execute("SELECT title,number FROM book")
+  @booklist = db.execute("SELECT * FROM book")
+  @booklist.each do |book|
+    artistName = db.execute("SELECT name FROM artist WHERE id = ?", book["artist_id"])
+    book["artist"] = artistName[0]["name"]
+  end
+  p @booklist
 end
 
-def hej()
-    puts "\e[32m______________________________________________________\e[0m"
+def hej(iterasion)
+    for i in 0...iterasion do
+      puts "\e[32m______________________________________________________\e[0m"
+    end
 end
 
 def db_loder()
@@ -121,22 +193,94 @@ def privliges(rool)
     {level: 0, href: '<a href="/place/new"> add place to book</a>'},
     {level: 0, href: '<a href="/pepol/new"> add pepol to book</a>'},
     {level: 1, href: '<a href="/book/edit"> chose a book to edit</a>'},
-    {level: 1, href: '<a href="/place/edit"> chose a place to edit</a>'},
-    {level: 1, href: '<a href="/pepol/edit"> chose a pepol to edit</a>'},
+    # {level: 1, href: '<a href="/place/edit"> chose a place to edit</a>'},
+    # {level: 1, href: '<a href="/pepol/edit"> chose a pepol to edit</a>'},
     {level: 2, href: '<a href="/rool/update"> chose a user to promot</a>'},
     {level: 3, href: '<a href="/rool/edit"> chose a user whoms roll to change</a>'},
-    {level: 4, href: '<a href="/rool/edit"> chose a user to edit</a>'}
+    {level: 4, href: '<a href="/user/edit"> chose a user to edit</a>'}
   ]
   all = ["writer","editor","promoter","moderator","admin"]
 
   routs = allrouts.select {|item| item[:level] <= all.index(rool) }.map! do |item|
     item[:href]
   end
+end
 
+def rolllrvel()
+  rool= session[:rool]
+  all = ["writer","editor","promoter","moderator","admin"]
+  return all.index(rool)
+end
+
+def editbook(title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs, artist, book_id)
+  db = db_loder()
+
+  # get artist_id
+  artists = db.execute("SELECT * FROM artist")
+  artists_id = 0
+  cunrentArtist = artist
+  artists.each do |artist|
+      if artist["name"] == cunrentArtist
+          artists_id = artist["id"]
+      end
+  end
+  if artists_id == 0
+      redirect("/error")
+  end 
+
+  db.execute("UPDATE book SET title=?, number=?, publish_date=?, posions_drunk=?, pirat_ship_shunk=?, wild_boar=?, menhirs=?, artist_id= ? WHERE id=?", title, number, publiDate, posionsDrunk, piratShipSunk, boar, menhirs, artists_id, book_id)
+
+  redirect("/home")
+end
+
+def editLinks()
+  hachOfBooks = getBooksInfo()
+  @bookHrefs = []
+  hej(1)
+  p hachOfBooks
+  hachOfBooks.each do |book|
+      @bookHrefs << "<a href=\"/books/#{book["id"]}/edit\">Edit: #{book["title"]}</a>" 
+  end
+end
+
+def promote(user_id)
+  db = db_loder()
+  db.execute('UPDATE user SET rool="writer" WHERE id=?',user_id)
+
+  redirect("/home")
+end
+
+def moderate(user_id,newRool)
+  db = db_loder()
+  db.execute('UPDATE user SET rool=? WHERE id=?',newRool,user_id)
+
+  redirect("/home")
 
 end
 
-# puts privliges("sdfhkl")
+def getReaderList()
+  db= db_loder()
+  allrouts = []
+  list = db.execute('SELECT name,id FROM user WHERE rool = "reader"')
+  list.each do |user|
+    allrouts << "<a href='/rool/#{user["id"]}/update'>#{user["name"]} </a>"
+  end
+  return allrouts
+end
 
+def getUserList()
+  db = db_loder()
+  allrouts = []
+  @userlist = db.execute('SELECT * FROM user')
+  @userlist.each do |user|
+    if user["rool"] != "admin"
+      allrouts << "<form action='/rool/#{user["id"]}/update' method='post'><label for=#{user["name"]}>Choose a rool for #{user["name"]}:</label><select id=#{user["id"]} name='rool' size='5' multiple><option value='reader'>reader</option><option value='writer'>writer</option><option value='editor'>editor</option><option value='promoter'>promoter</option><option value='moderator'>moderator</option></select><input type='submit'><br>"
+    end
+  end
+  return allrouts
+end
 
-# addBookToPlaceInBook("mallorca",2)
+def deletUser(user_id)
+  db = db_loder()
+  db.execute('DELETE FROM user WHERE id=?',user_id)
+end
